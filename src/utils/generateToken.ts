0bf9b39a16c7a78d69f91de6c1a75de365529d44
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2025-01-10 00:19:31 +0600

    Refactor generateAccessToken to increase access token expiration time

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -5,7 +5,7 @@ const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 const REFRESH_SECRET = process.env.REFRESH_SECRET || 'your_refresh_secret';
 
 export const generateAccessToken = (payload:any): string => {
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: '15m' }); // Short-lived access token
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: '150m' }); // Short-lived access token
 };
 
 export const generateRefreshToken = (payload: any): string => {
